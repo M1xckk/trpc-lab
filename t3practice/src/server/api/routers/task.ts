e4d2f40c92f133270e85ef483fa3be30952f8988
Brian Xu <mattx0121@gmail.com>
2023-03-27 11:11:36 -0700

    change theme name

--- t3practice/src/server/api/routers/task.ts
+++ t3practice/src/server/api/routers/task.ts
@@ -1,7 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import showTasks from "./taskHelpers";
 import createTask from "./taskHelpers/create";
-// import deleteTask from "./taskHelpers/deleteTask";
+ import deleteTask from "./taskHelpers/deleteTask";
 import toggleCompleted from "./taskHelpers/toggleCompleted";
 
 export const taskRouter = createTRPCRouter({
